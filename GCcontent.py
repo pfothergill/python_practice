40a889ecf67ff19753b83c7144fd43b3ad2c5477
Paul Fothergill <paul-fothergill@uiowa.edu>
2020-02-24 12:19:46 -0600

    updated script

--- GCcontent.py
+++ GCcontent.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-# here is our first function
+# this function will return the percentage of the 
+# amount of guanine and cytosine within a given
+# DNA sequence
 def GCcontent(dna):
     # our first function is called GCcontent and
     # accepts a single arguement called dna;
@@ -14,4 +16,4 @@ def GCcontent(dna):
     numT = dna.count("T")
     numA = dna.count("A")
 
-    return (numG + numC) / (numG + numC + numT + numA)
+    return ((numG + numC) / (numG + numC + numT + numA)) * 100
