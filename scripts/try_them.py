131c936c8f191bc7c003e97d26147142f83186cd
Paul Fothergill <paul-fothergill@uiowa.edu>
2020-02-24 15:23:23 -0600

    Changed try_them.py

--- scripts/try_them.py
+++ scripts/try_them.py
@@ -8,13 +8,49 @@ def foo2(x, y):
         return x
     return y
 
+def foo3(x, y, z):
+    if x > y:
+        tmp = y
+        y = x
+        x = tmp
+    if y > z:
+        tmp = z
+        z = y
+        y = tmp
+    if x > y:
+        tmp = y
+        y = x
+        x = tmp
+    return [x, y, z]
+
+def otherfoo3(x, y, z):
+    if x > y:
+        tmp = y
+        y = x
+        x = tmp
+    if y > z:
+        tmp = z
+        z = y
+        y = tmp
+    if x > y:
+        tmp = y
+        y = x
+        x = tmp
+    return [x, y, z]
 # Test foo1()
 print(foo1(4))
 print(foo1(9))
 print(foo1(100))
 
-# Test foo2()
+# # Test foo2()
 print(foo2(2, 4), 'x=2 y=4')
 print(foo2(3, 1), 'x=3 y=1')
 print(foo2(3, 3), 'x=3 y=3')
 
+# # Test foo3()
+print(foo3(1, 2, 3), otherfoo3(1, 2, 3))
+print(foo3(2, 4, 1), otherfoo3(2, 4, 1))
+print(foo3(4, 2, 1))
+print(foo3(4, 2, 5))
+print(foo3(16, 10, 13))
+print(foo3(1, 1, 1))
